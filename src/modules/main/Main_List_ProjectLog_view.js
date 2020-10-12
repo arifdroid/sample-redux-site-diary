fb73dbd46a782fc7e063cc4dde7fef82d605f198
arifzamri <arifdroiddeveloper@gmail.com>
2020-10-12 15:40:18 +0800

    add api calls

--- src/modules/main/Main_List_ProjectLog_view.js
+++ src/modules/main/Main_List_ProjectLog_view.js
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CardView from 'react-native-cardview';
 import MainButton from '../../common/components/main-button/MainButton';
 import { styles } from "../../common/styles";
+import { URL } from "@env"
+import axios from 'react-native-axios';
+import moment from "moment";
 
-const sample_data = [{ project_name: 'projek KLCC', date_log: '22/02/2020' }, { project_name: 'projek KLCC', date_log: '12/10/2020' }, { project_name: 'projek KLCC', date_log: '08/11/2020' }]
+const Main_List_ProjectLog_view = ({ navigation, route }) => {
 
-const Main_List_ProjectLog_view = ({navigation, route}) => {
+    useEffect(() => {
 
-    const __select =(item)=>{
+        _MainPageAPI();
+
+    }, [])
+
+    const [dataToRender, setDataToRender]= useState(null);
+
+    const _MainPageAPI = async () => {
+
+        try {
+
+             let data = await axios.get(`${URL}/api/list-site-logs`);
+
+            //  console.log('data is ', data)
+            setDataToRender(data.data);
+
+        } catch (error) {
+
+        }
+
+
+
+
+    }
+
+    const __select = (item) => {
         navigation.navigate('Form_1_View')
     }
 
-    const __pressLogin =()=>{
-        
+    const __pressLogin = () => {
+
         let new_form = true;
-        navigation.navigate('Form_1_View',{new_form})
+        navigation.navigate('Form_1_View', { new_form })
     }
 
     return (
@@ -28,8 +55,11 @@ const Main_List_ProjectLog_view = ({navigation, route}) => {
         >
             <Text style={{ alignSelf: 'flex-end', marginRight: 30, marginBottom: 10 }}>Engineer</Text>
             <Text style={{ marginLeft: 35, marginBottom: 10 }}>Most Recent</Text>
-            <FlatList
-                data={sample_data}
+            {
+                dataToRender === null?
+                <></>:
+                <FlatList
+                data={dataToRender}
                 renderItem={({ item }) => {
                     return (
                         <CardView
@@ -45,7 +75,7 @@ const Main_List_ProjectLog_view = ({navigation, route}) => {
                             cardMaxElevation={1}
                         >
                             <TouchableOpacity style={{ padding: 20, backgroundColor: 'white', shadowRadius: 20, borderRadius: 10, flex: 1 }}
-                                onPress={()=>__select(item)}
+                                onPress={() => __select(item)}
                             >
                                 <View style={{ flexDirection: 'row', flex: 1 }}>
                                     <Text style={{ flex: 2 }}>Project</Text>
@@ -53,7 +83,7 @@ const Main_List_ProjectLog_view = ({navigation, route}) => {
                                 </View>
                                 <View style={{ flexDirection: 'row', flex: 1 }}>
                                     <Text style={{ flex: 2 }}>{item.project_name}</Text>
-                                    <Text style={{ flex: 1 }}>{item.date_log}</Text>
+                                    <Text style={{ flex: 1 }}>{moment(item.createdAt).format("DD-MM-YYYY")}</Text>
                                 </View>
 
 
@@ -63,11 +93,13 @@ const Main_List_ProjectLog_view = ({navigation, route}) => {
                     )
                 }}
 
-            ></FlatList>
-        {/* <View style={{backgroundColor:'yellow'}}> */}
-        <MainButton onPress={__pressLogin} buttonStyle={styles.button_new_log} textStyle={styles.button_text_login} buttonText={'+ Create New Log'}></MainButton>
-        {/* </View> */}
-        
+            />
+            }
+          
+            
+            <MainButton onPress={__pressLogin} buttonStyle={styles.button_new_log} textStyle={styles.button_text_login} buttonText={'+ Create New Log'}></MainButton>
+            
+
 
         </SafeAreaView>
     )
